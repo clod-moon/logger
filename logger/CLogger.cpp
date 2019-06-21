9d0525f5defd289e8a1ad2ac3597aac181ae7c38
zhangyingchun <zhangyingchun@enst.org.cn>
2019-06-21 17:39:18 +0800

    修改CLooger

--- logger/CLogger.cpp
+++ logger/CLogger.cpp
@@ -1,12 +1,187 @@
 #include "CLogger.h"
+#include <assert.h>
+#include <thread>
 
+CLogger::LogLevel initLogLevel()
+{
+	if (::getenv("MUDUO_LOG_TRACE"))
+		return CLogger::TRACE;
+	else if (::getenv("MUDUO_LOG_DEBUG"))
+		return CLogger::DEBUG_;
+	else
+		return CLogger::INFO;
+}
 
+CLogger::LogLevel g_logLevel = initLogLevel();
 
-CLogger::CLogger()
+const char* LogLevelName[CLogger::NUM_LOG_LEVELS] =
+{
+  "TRACE ",
+  "DEBUG ",
+  "INFO  ",
+  "WARN  ",
+  "ERROR ",
+  "FATAL ",
+};
+
+// helper class for known string length at compile time
+class T
+{
+public:
+	T(const char* str, unsigned len)
+		:str_(str),
+		len_(len)
+	{
+		assert(strlen(str) == len_);
+	}
+
+	const char* str_;
+	const unsigned len_;
+};
+
+inline CLogStream& operator<<(CLogStream& s, T v)
+{
+	s.append(v.str_, v.len_);
+	return s;
+}
+
+inline CLogStream& operator<<(CLogStream& s, const CLogger::SourceFile& v)
+{
+	s.append(v.data_, v.size_);
+	return s;
+}
+
+void defaultOutput(const char* msg, int len)
+{
+	size_t n = fwrite(msg, 1, len, stdout);
+	//FIXME check n
+	(void)n;
+}
+
+void defaultFlush()
+{
+	fflush(stdout);
+}
+
+CLogger::OutputFunc g_output = defaultOutput;
+CLogger::FlushFunc g_flush = defaultFlush;
+std::string g_logTimeZone;
+
+CLogger::Impl::Impl(LogLevel level, int savedErrno, const SourceFile& file, int line)
+	: m_time(CMilliTimestamp::Now()),
+	m_sstream(),
+	m_nLevel(level),
+	m_nLine(line),
+	m_strBasename(file)
+{
+	formatTime();
+	m_sstream << std::this_thread::get_id();;
+	m_sstream << LogLevelName[level];
+	if (savedErrno != 0)
+	{
+		char buf[64];
+#ifdef _WIN32
+		strerror_s(buf, 64, savedErrno);
+#else
+		strerror_r(savedErrno,buf, 64);
+#endif 
+		m_sstream << buf << " (errno=" << savedErrno << ") ";
+
+	}
+}
+
+void CLogger::Impl::formatTime()
+{
+	int64_t microSecondsSinceEpoch = m_time.GetMilliseconds();
+	time_t seconds = static_cast<time_t>(microSecondsSinceEpoch / CMilliTimestamp::kMilliSecondsPerSecond);
+	int microseconds = static_cast<int>(microSecondsSinceEpoch % CMilliTimestamp::kMilliSecondsPerSecond);
+	if (seconds != t_lastSecond)
+	{
+		t_lastSecond = seconds;
+		struct tm tm_time;
+		if (g_logTimeZone.valid())
+		{
+			tm_time = g_logTimeZone.toLocalTime(seconds);
+		}
+		else
+		{
+			::gmm_timer(&seconds, &tm_time); // FIXME TimeZone::fromUtcTime
+		}
+
+		int len = snprintf(t_time, sizeof(t_time), "%4d%02d%02d %02d:%02d:%02d",
+			tm_time.tm_year + 1900, tm_time.tm_mon + 1, tm_time.tm_mday,
+			tm_time.tm_hour, tm_time.tm_min, tm_time.tm_sec);
+		assert(len == 17); (void)len;
+	}
+
+	if (g_logTimeZone.valid())
+	{
+		Fmt us(".%06d ", microseconds);
+		assert(us.length() == 8);
+		m_sstream << T(t_time, 17) << T(us.data(), 8);
+	}
+	else
+	{
+		Fmt us(".%06dZ ", microseconds);
+		assert(us.length() == 9);
+		m_sstream << T(t_time, 17) << T(us.data(), 9);
+	}
+}
+
+void CLogger::Impl::finish()
+{
+	m_sstream << " - " << m_strBasename << ':' << m_nLine << '\n';
+}
+
+CLogger::CLogger(SourceFile file, int line)
+	: m_impl(INFO, 0, file, line)
 {
 }
 
+CLogger::CLogger(SourceFile file, int line, LogLevel level, const char* func)
+	: m_impl(level, 0, file, line)
+{
+	m_impl.m_sstream << func << ' ';
+}
+
+CLogger::CLogger(SourceFile file, int line, LogLevel level)
+	: m_impl(level, 0, file, line)
+{
+}
+
+CLogger::CLogger(SourceFile file, int line, bool toAbort)
+	: m_impl(toAbort ? FATAL : ERROR, errno, file, line)
+{
+}
 
 CLogger::~CLogger()
 {
+	m_impl.finish();
+	const std::string& buf(stream().str());
+	g_output(buf.data(), buf.length());
+	if (m_impl.m_nLevel == FATAL)
+	{
+		g_flush();
+		abort();
+	}
+}
+
+void CLogger::setLogLevel(CLogger::LogLevel level)
+{
+	g_logLevel = level;
+}
+
+void CLogger::setOutput(OutputFunc out)
+{
+	g_output = out;
+}
+
+void CLogger::setFlush(FlushFunc flush)
+{
+	g_flush = flush;
+}
+
+void CLogger::setTimeZone(const std::string tz)
+{
+	g_logTimeZone = tz;
 }
--- logger/CLogger.h
+++ logger/CLogger.h
@@ -1,8 +1,127 @@
-#pragma once
+#ifndef _C_CLogger__
+#define _C_CLogger__
+
+#include "CLogStream.h"
+#include "CTimestamp.h"
+#include "CLogfile.h"
+
 class CLogger
 {
 public:
-	CLogger();
+	enum LogLevel
+	{
+		TRACE,
+		DEBUG_,
+		INFO,
+		WARN,
+		ERROR,
+		FATAL,
+		NUM_LOG_LEVELS,
+	};
+
+	typedef std::stringstream CLogStream;
+
+	// compile time calculation of basename of source file
+	class SourceFile
+	{
+	public:
+		template<int N>
+		SourceFile(const char(&arr)[N])
+			: data_(arr),
+			size_(N - 1)
+		{
+			const char* slash = strrchr(data_, '/'); // builtin function
+			if (slash)
+			{
+				data_ = slash + 1;
+				size_ -= static_cast<int>(data_ - arr);
+			}
+		}
+
+		explicit SourceFile(const char* filename)
+			: data_(filename)
+		{
+			const char* slash = strrchr(filename, '/');
+			if (slash)
+			{
+				data_ = slash + 1;
+			}
+			size_ = static_cast<int>(strlen(data_));
+		}
+
+		const char* data_;
+		int size_;
+	};
+
+	CLogger(SourceFile file, int line);
+	CLogger(SourceFile file, int line, LogLevel level);
+	CLogger(SourceFile file, int line, LogLevel level, const char* func);
+	CLogger(SourceFile file, int line, bool toAbort);
 	~CLogger();
+
+	CLogStream& stream() { return m_impl.m_sstream; }
+
+	static LogLevel logLevel();
+	static void setLogLevel(LogLevel level);
+
+	typedef void(*OutputFunc)(const char* msg, int len);
+	typedef void(*FlushFunc)();
+	static void setOutput(OutputFunc);
+	static void setFlush(FlushFunc);
+	static void setTimeZone(const std::string tz);
+
+private:
+
+	class Impl
+	{
+	public:
+		typedef CLogger::LogLevel LogLevel;
+		Impl(LogLevel level, int old_errno, const SourceFile& file, int line);
+		void formatTime();
+		void finish();
+
+		CMilliTimestamp m_time;
+		std::stringstream m_sstream;
+		LogLevel m_nLevel;
+		int m_nLine;
+		SourceFile m_strBasename;
+	};
+
+	Impl m_impl;
+
 };
 
+extern CLogger::LogLevel g_logLevel;
+
+inline CLogger::LogLevel CLogger::logLevel()
+{
+	return g_logLevel;
+}
+
+#define LOG_TRACE if (CLogger::logLevel() <= CLogger::TRACE) \
+  CLogger(__FILE__, __LINE__, CLogger::TRACE, __func__).stream()
+#define LOG_DEBUG if (CLogger::logLevel() <= CLogger::DEBUG) \
+  CLogger(__FILE__, __LINE__, CLogger::DEBUG, __func__).stream()
+#define LOG_INFO if (CLogger::logLevel() <= CLogger::INFO) \
+  CLogger(__FILE__, __LINE__).stream()
+#define LOG_WARN CLogger(__FILE__, __LINE__, CLogger::WARN).stream()
+#define LOG_ERROR CLogger(__FILE__, __LINE__, CLogger::ERROR).stream()
+#define LOG_FATAL CLogger(__FILE__, __LINE__, CLogger::FATAL).stream()
+#define LOG_SYSERR CLogger(__FILE__, __LINE__, false).stream()
+#define LOG_SYSFATAL CLogger(__FILE__, __LINE__, true).stream()
+
+
+#define CHECK_NOTNULL(val) \
+  ::CheckNotNull(__FILE__, __LINE__, "'" #val "' Must be non NULL", (val))
+
+template <typename T>
+T* CheckNotNull(CLogger::SourceFile file, int line, const char *names, T* ptr)
+{
+	if (ptr == NULL)
+	{
+		CLogger(file, line, CLogger::FATAL).stream() << names;
+	}
+	return ptr;
+}
+
+#endif  //_C_CLogger__
